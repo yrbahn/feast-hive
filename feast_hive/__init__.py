2bf1257f90e30cb342d405dc9c613747463f1342
root <root@gpu-cloud-cpu10.dakao.io>
2021-08-27 14:07:22 +0900

    refactoring

--- feast_hive/__init__.py
+++ feast_hive/__init__.py
@@ -1,4 +1,5 @@
 from .hive_source import HiveOptions, HiveSource
 from .hive import HiveOfflineStore, HiveOfflineStoreConfig
 
+
 __all__ = ["HiveOptions", "HiveSource", "HiveOfflineStoreConfig", "HiveOfflineStore"]
